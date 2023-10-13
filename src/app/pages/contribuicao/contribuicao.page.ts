0f40de28e0b2a651dec08e2eb494a17bb4164967
Amilton Santos <a1000ton1000ton@gmail.com>
2023-10-13 15:25:34 -0300

    :art: implementa tela de contribuicao

--- src/app/pages/contribuicao/contribuicao.page.ts
+++ src/app/pages/contribuicao/contribuicao.page.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-contribuicao',
-  templateUrl: './contribuicao.page.html',
-  styleUrls: ['./contribuicao.page.scss'],
+    selector: 'app-contribuicao',
+    templateUrl: './contribuicao.page.html',
+    styleUrls: ['./contribuicao.page.scss'],
 })
-export class ContribuicaoPage implements OnInit {
+export class ContribuicaoPage {
+    chavePixDizimo: string = '00184519000177';
+    chavePixOferta: string = 'parceirosmissionarios@hotmail.com';
 
-  constructor() { }
+    constructor(private toastController: ToastController) { }
 
-  ngOnInit() {
-  }
+    copyToClipboardChavePix(msg: number) {
+        if(msg === 1) {
+            navigator.clipboard.writeText(this.chavePixDizimo);
+        } else {
+            navigator.clipboard.writeText(this.chavePixOferta);
+        }
+        this.presentToast('bottom');
+    }
+
+    async presentToast(position: 'top' | 'bottom' | 'bottom') {
+        const toast = await this.toastController.create({
+          message: 'Pix Copiado!',
+          duration: 1500,
+          position: position,
+          cssClass: 'toast-custom-pix',
+        });
+    
+        await toast.present();
+      }
 
 }
